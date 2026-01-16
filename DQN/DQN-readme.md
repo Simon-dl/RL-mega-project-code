926e04801b852eb7c3a3d5f8e49648a8438280e0
Simon <simhlwy@gmail.com>
2026-01-16 10:07:26 -0600

    inital buffer pop function WIP

--- DQN/DQN-readme.md
+++ DQN/DQN-readme.md
@@ -5,11 +5,16 @@ First I need the preprocessing function phi, for stacking m frames, gray scaling
 
 Then build out model as described in paper. (done) 1-14-26
 
-Set up the random runs and make sure populating the replay buffer works as expected. remember eps annealing
+Set up the random runs and make sure populating the replay buffer works as expected. 
 
 do a test batch gradient desecent on the network using samples from replay buffer, make sure loss and sampling is done correctly.
 
-Then set up dummy training loop with simple hyperparams to make sure it behaves all togeher correctly on a small scale.
-Paper does network SGD update every 4 actions.
+Then set up dummy training loop with simple hyperparams to make sure it behaves all togeher correctly on a small scale. remember eps annealing. Paper does network SGD update every 4 actions.
 
 Then set up recordings and do a full scale training and hope it works.
+
+
+
+Implementation notes: 
+
+- Gonna set phi_2 = -1 if game terminated in the frames getting to phi_2
--- DQN/main.py
+++ DQN/main.py
@@ -8,11 +8,66 @@ from DQN import DQN
 import torch
 
 
+
+def populate_buffer(env,frame_skip):
+    """
+    Takes in env and number of frames to skip.
+
+    returns: phi_2 and starting_frame_count
+    """
+    phi_1 = 0
+    phi_2 = 0
+
+    #phi_1 = torch.tensor(phi(frames),dtype=torch.float)
+
+   
+    #populate replay buffer with random plays, split into helper function later
+    frames = []
+    observations = env.reset()
+    frames.append(observations)
+    total_frame_count += 1
+    old_action = 0
+    old_reward = 0
+
+    action = 0 #start with no action
+
+    for i in range(3): #setting up
+        total_frame_count += 1
+        observation, reward, terminated, truncated = env.step(action)
+        frames.append(observations)
+        if len(frames) == frame_skip:
+            action = env.action_space.sample() 
+            phi_1 = phi(frames)
+            phi_2 = phi_1
+
+            old_action = action
+            old_reward = reward
+            frames = []
+
+    while total_frame_count <= 10:
+        observation, reward, terminated, truncated = env.step(action)
+        frames.append(observation)
+        total_frame_count += 1
+
+        if len(frames) == frame_skip:
+            phi_1 = phi_2
+            phi_2 = phi(frames) 
+            transition = (phi_1,old_action,old_reward,phi_2)
+            replay_buffer.append(transition)
+
+            action =  env.action_space.sample() 
+            old_action = action
+            old_reward = reward
+            frames = []
+
+
+
 def breakout_training():
     #render_mode="human" for when I want to watch an episode
     env = gym.make('ALE/Breakout-v5')
 
-    replay_memory = []
+    replay_buffer = []
+    total_frame_count = 0 
 
     behavior_model = DQN(4)
     target_model = DQN(4)
@@ -24,24 +79,27 @@ def breakout_training():
 
     episodes = 1
     discount = .99
+    total_frame_count = 0 
+
+
+    episode_over = False
+    total_reward = 0
+
+    frame_skip = 4
+
+    phi_1 = 0
+    phi_2 = 0
 
 
     for i in range(episodes): 
-        episode_over = False
-        total_reward = 0
 
-        frame_skip = 4
-
-
-        phi_1 = 0
-        phi_2 = 0
 
 
 
         
         #get inital proccessed frames
         frames = []
-        action = 0 #start with no action
+        action = action = env.action_space.sample() 
         observations = env.reset()
         frames.append(observations)
         for j in range(3):
